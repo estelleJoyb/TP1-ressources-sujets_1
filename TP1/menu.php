90405cbcd8dd9affc2b73377345629d10cd3da15
estelleJoyb <estelle.joyb@gmail.com>
2022-10-18 11:26:07 +0200

    suite css

---
+++ TP1/menu.php
@@ -0,0 +1,18 @@
+<div class="menu">
+    <h1 class="titre_menu">Travel Discount</h1>
+    <ul class="nav">
+        <li><a href="index.html">Accueil</a></li>
+        <li class="dropdown"><a href="javascript:void(0)" class="dropbtn">Espace perso</a>
+            <div class="dropdown-content">
+                <a href="perso.html">Connexion</a>
+                <a href="perso.html">Mes Informations</a>
+                <a href="perso.html">Messagerie</a>
+                <a href="perso.html">Historique</a>
+            </div>
+        </li>
+        <li><a href="destinations.html">Destinations</a></li>
+        <li><a href="audio.html">Voyage virtuel Audio</a></li>
+        <li><a href="video.html">Voyage virtuel vidéo</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</div>
